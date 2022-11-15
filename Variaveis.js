06cb0933633e2f9e9b3cc963598400a490500b4f
Alexandre Bittencourt <alexandre_bro@hotmail.com>
2022-11-15 18:32:01 -0300

    Atualização aula Variáveis - Parte II

--- Variaveis.js
+++ Variaveis.js
@@ -1,3 +1,4 @@
+//Aula Tipos de Dados
 //Declarar
 var nome = "Luan"
 console.log(nome)
@@ -6,8 +7,9 @@ console.log(nome)
 nome = "Monteiro"
 console.log(nome)
 
+//Aula Variáveis - Parte I
 /*Supostamente no caso abaixo aconteceria o que é chamado de "Hoisting", que seria transformar a variável local "nome2" em
-uma variável global, o que também só ocorreria com variáveis do topo "var". Assim esperava-se que a primeira chamada do "console.log" retornasse o valor "Luan", que foi declarado na sequência do código. Porém o hoisting não ocorreu.*/
+uma variável global, o que também só ocorreria com variáveis do tipo "var". Assim esperava-se que a primeira chamada do "console.log" retornasse o valor "Luan", que foi declarado na sequência do código. Porém o hoisting não ocorreu.*/
 
 //Teste de declarações de variáveis
 console.log('Primeiro teste da variável "nome2":', nome2)
@@ -24,3 +26,46 @@ console.log('Primeiro teste da variável "nome3":', nome3)
     console.log('Segundo teste da variável "nome3":', nome3)
 }
 console.log('Terceiro teste da variável "nome3":', nome3)
+
+//Aula Variáveis - Parte II
+//Objetos
+const human = {
+    nome: "Luan Monteiro",
+    age: 30,
+    weight: 90,
+    isAthlete: true,
+    walk: function walking() {
+        console.log('Ando bastante!')
+    }
+}//Variável do tipo Objeto
+console.log(human)//Impressão de todos os valores do objeto
+console.log({human})//Impressão de objeto
+console.log(human.nome)//Impressão de parâmetros do objeto
+console.log(human.age)
+console.log(human.weight)
+console.log(human.isAthlete)
+console.log(human.walk())
+//Concatenação
+//Faz-se a concatenação de strings com o uso do conector "+"
+console.log("O humano " + human.nome + " tem a idade " + human.age)
+//Ingerpolação: a interpolação tema guinte sintaxe (entre crases)
+console.log(`O humano ${human.nome} tem a idade ${human.age} anos.`)
+//Concatenação e interpolação tem idéias semelhantes tendo como difereça basicamente a sintaxe.
+
+//Array (vetor)
+const animals = [
+    "Elefante", //Posição 0
+    "Gato", //Posição 1
+    "Leão", //Posição 2
+    "Cachorro", //Posição 3
+    {//Posição 4
+        nome: "Aegon",
+        age: 4,
+        weight: 28,
+        type: "Cachorro",
+        racial: "Samoyeda"
+    }
+]
+console.log(animals)
+console.log(animals.length)//Imprimir o tamanho do vetor
+console.log(animals[4])//Imprimir posição selecionada
